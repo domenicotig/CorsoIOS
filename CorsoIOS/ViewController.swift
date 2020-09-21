c83afe38a0369e1550fd1dac2212cd629b4db89d
Domenico Tigano <domenicotig@gmail.com>
2020-09-21 15:30:12 +0200

    Initial Commit

---
+++ CorsoIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CorsoIOS
+//
+//  Created by Domenico Tigano on 21/09/2020.
+//  Copyright © 2020 Domenico Tigano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
